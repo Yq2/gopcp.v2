bcf7d65acca6426f2a60d6dbf484bda138fdf3ca
yangqiang <1225807604@qq.com>
2019-03-23 20:56:14 +0800

    增加break语句演示

---
+++ chapter7/break_line/demo1.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("break 语句规则展示")
+L1:
+	for i := 0; ; i++ {
+		for j := 0; ; j++ {
+			if i > 5 {
+				break L1
+			}
+			if j > 10 {
+				break
+			}
+			fmt.Printf("i=%d,j=%d\n", i, j)
+		}
+	}
+
+	fmt.Println("contine 语句规则展示")
+	L2:
+		for i :=0 ;;i++{
+			for j := 0;;j++ {
+				if i >= 5 {
+					continue L2
+				}
+				if j > 10 {
+					continue
+				}
+				fmt.Printf("i=%d,j=%d\n", i, j)
+			}
+	}
+		fmt.Println("展示结束")
+}
